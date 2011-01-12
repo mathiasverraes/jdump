3ed3b9012f456bcf235bba298730e0b3c21d8b97
Mathias Verraes <mathias@verraes.net>
2011-01-12 09:41:33 +0000

    fixed [#17882] dumpBacktrace(); documentation error

--- src/component/admin/readme.php
+++ src/component/admin/readme.php
@@ -34,7 +34,7 @@ Shortcuts
   Use inside a template's index.php to dump the parameters.
 - dumpMessage( 'Your message' );
   Displays a custom message. Very handy to check if a function or a loop is executed etc...
-- dumpBacktrace();
+- dumpTrace();
   Displays the backtrace.
 
 
