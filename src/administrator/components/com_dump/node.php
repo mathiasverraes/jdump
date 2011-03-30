b9f54c0dad64ff3f0b5ce30dd0b6bc4896e713ae
Mathias Verraes <mathias@verraes.net>
2011-03-30 08:33:08 +0200

    implemented ivo's proposed patch

--- src/administrator/components/com_dump/node.php
+++ src/administrator/components/com_dump/node.php
@@ -52,13 +52,48 @@ class DumpNode {
                 $node['classname'] = get_class( $var );
                 break;
 
-            case 'properties':
-                $object_vars = get_object_vars( $var );
-                ksort($object_vars);
+                       case 'properties':
+                $object_vars = get_object_vars( $var ) ;
                 foreach ( $object_vars as $key => $value ) {
-                    $node['children'][] = & DumpNode::getNode( $value, $key, null, $level + 1 );
+                    $node['children'][] = & DumpNode::getNode( $value,
+$key, null, $level + 1 );
+                }
+                // getter-based properties
+                $methods = get_class_methods( $var ) ;
+                foreach ( $methods as $name ) {
+                      if( 4 <= strlen($name) && 0 == strncmp('get',
+$name, 3) && ctype_upper($name[3]) ) {
+                           $child = & DumpNode::getNode( $var, $name,
+'getter', $level + 1 );
+                           if( $child )
+                              $node['children'][] = & $child;
+                      }
                 }
                 break;
+
+            case 'getter':
+                $property = substr( $name, 3 );
+                if( 2 <= strlen($property) && !ctype_upper($property[1]) )
+                    $property[0] = strtolower( $property[0] );
+                if( property_exists($var, $property) ||
+property_exists($var, '_'.$property) )
+                    return false;  // no duplicate entry for getters
+                  $getter = new ReflectionMethod( $var, $name );
+                if( 0 == $getter->getNumberOfRequiredParameters() ) {
+                       try {
+                         $value = call_user_func( array($var, $name) );
+                       } catch(Exception $e) {
+                           $value = $e->getMessage();
+                       }
+                    $node = & DumpNode::getNode( $value, $property,
+null, $level );
+                }
+                else {
+                    $node['name'] = $property;
+                    $node['value'] = null;
+                    $node['type'] = 'unknown';
+                }
+                   break;
 
             case 'methods':
                 $methods = get_class_methods( $var ) ;
